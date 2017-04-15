f78897ad211870935701b5a9c2468a2b6cdd0e47
Peter Ellis <peter.ellis2013nz@gmail.com>
2017-04-16 00:24:12 +1200

    working version of shiny app including setting seats assumptions

--- nz-election-2017/server.R
+++ nz-election-2017/server.R
@@ -10,47 +10,48 @@ load("parties.rda")
 
 n <- 5000 
 
+
+# a filler data frame of the three parties that don't get any simulated electorate seats.
+# Note that NZ First isn't really a certainty to get their seat, but it doesn't matter as they are 
+# almost certainly above the 5% threshold anyway, so we don't bother to simulate Northland
+filler <- data.frame(
+  party = c("Conservative", "Green", "NZ First"),
+  seats = c(0, 0, 1),
+  sim = rep(1:n, each = 3)
+)
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   parties2 <- reactive({
-    gsub("M.ori", "Maori", input$coal_members)
+    tmp <- gsub("M.ori", "Maori", input$coal_members)
+    return(tmp)
   })
   
   #---------------simulate seats----------------
-  m_votes_2014 <- data_frame(
-    labour = c(9712, 7533, 8089, 9753, 8445, 12191, 5837),
-    mana_maori = c(11548, 8695, 8475, 8928, 6887, 7612, 15208)
-  ) %>%
-    mutate(
-      mana_maori = mana_maori * 0.9,
-      prob_lab = labour / (mana_maori + labour),
-      prob_oth = 1 - prob_lab)
-  
-  
-  # a filler data frame of the three parties that don't get any simulated electorate seats.
-  # Note that NZ First isn't really a certainty to get their seat, but it doesn't matter as they are 
-  # almost certainly above the 5% threshold anyway, so we don't bother to simulate Northland
-  filler <- data.frame(
-    party = c("Conservative", "Green", "NZ First"),
-    seats = c(0, 0, 1),
-    sim = rep(1:n, each = 3)
-  )
+  maori_probs <- reactive({
+    tmp <- data.frame(labour = c(input$m1, input$m2, input$m3, 
+                                 input$m4, input$m5, input$m6, input$m7)) %>%
+      mutate(other = 1 - labour)
+    print(tmp)
+    return(tmp)
+  })
+
   
   # see https://en.wikipedia.org/wiki/M%C4%81ori_electorates for the true names of the Maori electorates.
   # For convenience I call them m1 : m7.
   # Also note - the procedure below often gives zero electorate seats to National.  This doesn't
   # impact on seat allocation as they always exceed the 5% party vote threshhold; they only
   # need to be in the simulation at all for the off chance they take Epsom off ACT.
-  electorate_sims <- data_frame(
-    orahiu = sample(c("United Future", "Labour"), prob = c(0.6, 0.4), size = n, replace = TRUE),
-    epsom = sample(c("ACT", "National", "Labour"), prob = c(0.8, 0.1, 0.1), size = n, replace = TRUE),
-    m1 = sample(c("Labour", "Mana"), prob = m_votes_2014[1, 3:4], size = n, replace = TRUE),
-    m2 = sample(c("Labour", "Maori"), prob = m_votes_2014[2, 3:4], size = n, replace = TRUE),
-    m3 = sample(c("Labour", "Maori"), prob = m_votes_2014[3, 3:4], size = n, replace = TRUE),
-    m4 = sample(c("Labour", "Maori"), prob = m_votes_2014[4, 3:4], size = n, replace = TRUE),
-    m5 = sample(c("Labour", "Maori"), prob = m_votes_2014[5, 3:4], size = n, replace = TRUE),
-    m6 = sample(c("Labour", "Maori"), prob = m_votes_2014[6, 3:4], size = n, replace = TRUE),
-    m7 = sample(c("Labour", "Maori"), prob = m_votes_2014[7, 3:4], size = n, replace = TRUE)
+  electorate_sims <- reactive({data_frame(
+    orahiu = sample(c("United Future", "Labour"), prob = c(input$ohariu, 1-input$ohariu), size = n, replace = TRUE),
+    epsom = sample(c("ACT", "National"), prob = c(input$epsom, 1-input$epsom), size = n, replace = TRUE),
+    m1 = sample(c("Labour", "Maori"), prob = maori_probs()[1, 1:2], size = n, replace = TRUE),
+    m2 = sample(c("Labour", "Maori"), prob = maori_probs()[2, 1:2], size = n, replace = TRUE),
+    m3 = sample(c("Labour", "Maori"), prob = maori_probs()[3, 1:2], size = n, replace = TRUE),
+    m4 = sample(c("Labour", "Maori"), prob = maori_probs()[4, 1:2], size = n, replace = TRUE),
+    m5 = sample(c("Labour", "Mana"),  prob = maori_probs()[5, 1:2], size = n, replace = TRUE),
+    m6 = sample(c("Labour", "Maori"), prob = maori_probs()[6, 1:2], size = n, replace = TRUE),
+    m7 = sample(c("Labour", "Maori"), prob = maori_probs()[7, 1:2], size = n, replace = TRUE)
   ) %>%
     mutate(sim = 1:n()) %>%
     gather(seat, party, -sim) %>%
@@ -58,24 +59,27 @@ shinyServer(function(input, output) {
     summarise(seats = n()) %>%
     ungroup() %>%
     rbind(filler) %>%
-    spread(party, seats, fill = 0) 
+    spread(party, seats, fill = 0)
+  })
   
-  seats <- t(sapply(1:n, function(i){
+  seats <- reactive({t(sapply(1:n, function(i){
     allocate_seats(votes = as.numeric(sims[i, ]), 
-                   electorate = as.numeric(electorate_sims[i, -1]),
-                   parties = parties)$seats_v
+                   electorate = as.numeric(electorate_sims()[i, -1]),
+                   parties = gsub("M.ori", "Maori", parties))$seats_v
   })) %>%
     as_tibble()
+  })
   
   
-  
-  seats_long <- seats %>%
+  seats_long <- reactive({
+    seats() %>%
     mutate(sim = 1:n()) %>%
     gather(party, seats, -sim) 
+  })
   
   #--------------build coalitions-------------
   the_seats <- reactive({
-    seats_long %>%
+    seats_long() %>%
     group_by(sim) %>%
     summarise(coal_seats = sum(seats[ party %in% parties2()]),
               coal_perc = coal_seats / sum(seats)) %>%
--- nz-election-2017/ui.R
+++ nz-election-2017/ui.R
@@ -3,6 +3,9 @@ library(ggvis)
 
 load("parties.rda")
 
+minsl <- 0.05
+maxsl <- 0.95
+
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
@@ -19,17 +22,25 @@ shinyUI(fluidPage(
          
        ),
        hr(),
-       sliderInput("epsom", "Epsom - probability of ACT win", min = 0, max = 1, value = 0.6),
-       sliderInput("ohariu", "Ohariu - probability of United Future win", min = 0, max = 1, value = 0.6)
+       HTML("<p>The choices of parties for a coalition above lead to predictions of the number of 
+seats in Parliament.   Party vote is simulated based on the model described at 
+<a href='http://ellisp.github.io/elections/elections.html'>Peter's Stats Stuff</a>.
+Conversion of party vote to number of seats requires assumptions of some key electorate outcomes.
+Adjusting the sliders for the electorates below and to the right will create new simulations of
+electorate seats, and hence of total seats allocated.</p>"),
+       
+       hr(),
+       sliderInput("epsom", "Epsom - probability of ACT win", min = minsl, max = maxsl, value = 0.8),
+       sliderInput("ohariu", "Ohariu - probability of United Future win", min = minsl, max = maxsl, value = 0.6)
        ),
     column(3, 
-       sliderInput("m1", "Ikaroa-Rawhiti - probability of Labour win", min = 0, max = 1, value = 0.6),
-       sliderInput("m2", "Tainui - probability of Labour win", min = 0, max = 1, value = 0.6),
-       sliderInput("m3", "Tamaki Makaurau - probability of Labour win", min = 0, max = 1, value = 0.6),
-       sliderInput("m4", "Te Tai Hauauru - probability of Labour win", min = 0, max = 1, value = 0.6),
-       sliderInput("m5", "Te Tai Tokerau - probability of Labour win", min = 0, max = 1, value = 0.6),
-       sliderInput("m6", "Te Tai Tonga - probability of Labour win", min = 0, max = 1, value = 0.6),
-       sliderInput("m7", "Wairaki - probability of Labour win", min = 0, max = 1, value = 0.6)
+       sliderInput("m1", "Ikaroa-Rawhiti - probability of Labour win", min = minsl, max = maxsl, value = 0.48),
+       sliderInput("m2", "Tainui - probability of Labour win", min = minsl, max = maxsl, value = 0.49),
+       sliderInput("m3", "Tamaki Makaurau - probability of Labour win", min = minsl, max = maxsl, value = 0.51),
+       sliderInput("m4", "Te Tai Hauauru - probability of Labour win", min = minsl, max = maxsl, value = 0.55),
+       sliderInput("m5", "Te Tai Tokerau - probability of Labour win", min = minsl, max = maxsl, value = 0.58),
+       sliderInput("m6", "Te Tai Tonga - probability of Labour win", min = minsl, max = maxsl, value = 0.64),
+       sliderInput("m7", "Wairaki - probability of Labour win", min = minsl, max = maxsl, value = 0.30)
     ),
     column(6,
            

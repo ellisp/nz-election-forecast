7d9c8dd86b30188964d0b2a3152596d080b91270
Peter Ellis <peter.ellis2013nz@gmail.com>
2017-06-24 10:53:05 +1200

    make installing latest nzelect the default

--- integrate.R
+++ integrate.R
@@ -1,6 +1,6 @@
 # Install latest version of nzelect with most recent polling data
 # (more up to date than the CRAN version)
-# devtools::install_github("ellisp/nzelect/pkg1")
+devtools::install_github("ellisp/nzelect/pkg1")
 
 library(tidyverse)
 library(magrittr)
